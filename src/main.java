ece73c10d680d80f24d721d29113983919e29a64
Filip Nachov <fnachov@gmail.com>
2024-09-18 17:45:14 +0200

    added a special title string var and made it set seperatly

--- src/main.java
+++ src/main.java
@@ -4,12 +4,13 @@ import java.awt.FlowLayout;
 
 class Main {
     public static void main(String[] args) {
-        JFrame window = new JFrame("JFRS");
+        String TITLE = "JFRS"; 
+
+        JFrame window = new JFrame();
         window.setLayout(new FlowLayout());
 
         window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
-        window.pack();
-
+        window.setTitle(TITLE);
         window.setSize(500, 500);
         window.setVisible(true);
     }
