2602d5a67c57b8e7c9be2412d950b8ae67290c89
Filip Nachov <fnachov@gmail.com>
2024-09-18 15:01:32 +0200

    added a include section and imported the needed things

--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
-public class Main{
+// imports
+import  javax.swing.JFrame;
+import java.awt.FlowLayout;
+
+public class Main {
     public static void main() {
-        // code should be here
+        // code
     }
 }
